1e8669b545931b3a696c23edecfdb94e042803f9
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2023-12-25 13:36:11 -0800

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+
+
+function App() {
+  
+
+  return (
+    <div className=" mx-auto bg-slate-800 h-screen w-screen">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Header = () => {
+
+
+    return (
+        <div className="container mx-auto">
+            <div>
+                <h1>Christian Alexander</h1>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
