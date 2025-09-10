156196c1f568254eef082037d2cc22307fa98ade
WulfCoDev <wulfcodev@gmail.com>
2025-09-10 16:19:51 -0700

    styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,21 +4,21 @@ export default function Home() {
       id="home"
       className="min-h-screen flex flex-col justify-center items-center text-center px-6 bg-[url(/hero-bg3.jpg)] bg-cover bg-no-repeat text-[#EDF2F4]"
     >
-      <h1 className="text-5xl font-bold mb-4">Hi, I’m Christian Alexander</h1>
-      <p className="text-lg text-[#EDF2F4] mb-6">
+      <h1 className="text-5xl md:text-6xl font-bold mb-4">Hi, I’m Christian Alexander</h1>
+      <p className="text-lg md:text-xl text-[#EDF2F4] mb-6">
         IT Specialist & Aspiring Software Engineer | CompTIA Triad Certified
       </p>
       <div className="space-x-4">
         <a
           href="#projects"
-          className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700"
+          className="px-6 py-3 bg-blue-600 hover:bg-[#ffb81c] text-white rounded-lg shadow "
         >
           View Projects
         </a>
         <a
           href="/Christian_Alexander_IT_Resume.pdf"
           download={true}
-          className="px-6 py-3 border border-blue-600 rounded-lg hover:bg-blue-50"
+          className="px-6 py-3 border border-blue-600 rounded-lg hover:bg-blue-50 hover:text-[#003DA5] drop-shadow-2xl drop-shadow-[#003DA5]"
         >
           Download Resume
         </a>
