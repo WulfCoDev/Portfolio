b6637239be9b6014d1274a9f01a849477d0375ac
WulfCoDev <wulfcodev@gmail.com>
2025-09-09 21:25:53 -0700

    styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,9 @@
 export default function About() {
   return (
-    <section id="about" className="py-20 bg-[#e6681a] min-h-screen">
+    <section id="about" className="py-20 bg-linear-to-t from-[#FFB81C] to-[#003DA5] min-h-screen">
       <div className="max-w-4xl mx-auto px-6 text-center">
-        <h2 className="text-3xl font-bold mb-6 text-[#edf2f4]">About Me</h2>
-        <p className="text-[#EDF2F4] leading-relaxed text-2xl">
+        <h2 className="text-4xl font-bold mb-6 text-[#edf2f4]">About Me</h2>
+        <p className="text-[#EDF2F4] leading-relaxed text-3xl">
           I’m an IT and software enthusiast with a background in Computer
           Science and Physics. I hold the CompTIA triad (A+, Network+,
           Security+) and enjoy building projects that combine problem-solving
