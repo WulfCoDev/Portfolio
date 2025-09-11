eb23abe9143c7e2790e6394b76765bd0bd83eaaf
WulfCoDev <wulfcodev@gmail.com>
2025-09-11 14:41:27 -0700

    styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
     <section id="contact" className="py-20 bg-linear-to-b from-[#FFB81C] to-[#003DA5] min-h-screen flex flex-col justify-center items-center">
       <div className="max-w-3xl mx-auto px-6 text-center ">
-        <h2 className="text-6xl font-bold mb-6">Get in Touch</h2>
+        <h2 className="text-6xl font-bold mb-6 text-outline">Get in Touch</h2>
         <p className="text-gray-700 mb-8 text-2xl">
           Feel free to reach out for collaborations, questions, or opportunities!
         </p>
@@ -14,9 +14,9 @@ export default function Contact() {
             <MdEmail />
             <a
               href="mailto:Christian.Alexander.Dev@gmail.com"
-              className="text-blue-600"
+              className="text-[#FFB81C]"
             >
-              Christian.Alexander.Dev@gmail.com
+              christian.alexander.dev@gmail.com
             </a>
           </div>
           <div className="flex items-center justify-center space-x-2">
@@ -24,7 +24,7 @@ export default function Contact() {
             <a
               href="https://www.linkedin.com/in/dev-christian-alexander/"
               target="_blank"
-              className="text-blue-600"
+              className="text-[#FFB81C]"
             >
               linkedin.com/in/dev-christian-alexander
             </a>
@@ -34,7 +34,7 @@ export default function Contact() {
             <a
               href="https://github.com/WulfCoDev"
               target="_blank"
-              className="text-blue-600"
+              className="text-[#FFB81C]"
             >
               github.com/WulfCoDev
             </a>
